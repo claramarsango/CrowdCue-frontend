4f1a6d2f3fa35f9903a1846b1e67fcc05160429a
claramarsango <claraxmarsango@gmail.com>
2023-03-14 15:58:22 +0100

    import css variables and fonts to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/fonts.css';
+import './styles/variables.css';
 import './index.css';
 
 const container = document.getElementById('root')!;
@@ -14,7 +16,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
