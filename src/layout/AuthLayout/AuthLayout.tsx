0f7f4dd716e5e3c3b5c24bc01f59847003637379
claramarsango <claraxmarsango@gmail.com>
2023-03-14 17:04:37 +0100

    add header to auth layout with logo and title

---
+++ src/layout/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import { Header } from './AuthLayoutStyled';
+
+const AuthLayout = () => {
+  return (
+    <Header>
+      <img
+        src="crowdcue-logo.png"
+        alt="CrowdCue logo"
+        className="header__logo"
+      />
+      <h1 className="header__title">CrowdCue</h1>
+    </Header>
+  );
+};
+
+export default AuthLayout;
