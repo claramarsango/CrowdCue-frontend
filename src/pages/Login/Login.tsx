dc1bb11032b2e5f4a6057e4577e6ae47262b25c0
claramarsango <claraxmarsango@gmail.com>
2023-03-15 12:17:05 +0100

    create login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '../../features/loginForm/LoginForm';
+import { PageTitle } from '../../shared/styles/PageTitleStyled';
+const Login = () => {
+  return (
+    <>
+      <PageTitle>LOG IN</PageTitle>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
