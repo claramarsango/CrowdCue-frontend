2e484480918b673c00bdc5bb0a82079acd65d4e9
claramarsango <claraxmarsango@gmail.com>
2023-03-15 12:38:44 +0100

    add log in form reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import loginFormReducer from '../features/loginForm/loginFormSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    loginForm: loginFormReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
