157f772de59d2183016c1879c83a4793f2ce0b94
claramarsango <claraxmarsango@gmail.com>
2023-03-15 12:15:56 +0100

    create shared pge title styled component

---
+++ src/shared/styles/PageTitleStyled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PageTitle = styled.h2`
+  display: none;
+
+  @media (min-width: 1440px) {
+    display: block;
+    font-size: var(--desktop-text-size-xl);
+  }
+`;
