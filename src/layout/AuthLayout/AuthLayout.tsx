04935e4ebf3e7b6d53f76469ef43611b9df6ec1c
claramarsango <claraxmarsango@gmail.com>
2023-03-15 12:12:00 +0100

    create authentication pages layout

--- src/layout/AuthLayout/AuthLayout.tsx
+++ src/layout/AuthLayout/AuthLayout.tsx
@@ -1,15 +1,21 @@
+import { Outlet } from 'react-router-dom';
 import { Header } from './AuthLayoutStyled';
 
 const AuthLayout = () => {
   return (
-    <Header>
-      <img
-        src="crowdcue-logo.png"
-        alt="CrowdCue logo"
-        className="header__logo"
-      />
-      <h1 className="header__title">CrowdCue</h1>
-    </Header>
+    <>
+      <Header>
+        <img
+          src="crowdcue-logo.png"
+          alt="CrowdCue logo"
+          className="header__logo"
+        />
+        <h1 className="header__title">CrowdCue</h1>
+      </Header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
