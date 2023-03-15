6ff4cc7f03fac926440475b76df45b98f8111857
claramarsango <claraxmarsango@gmail.com>
2023-03-15 12:13:07 +0100

    add authentication layout to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AuthLayout from '../layout/AuthLayout/AuthLayout';
+import Login from '../pages/Login/Login';
 
 export const router = createBrowserRouter([
-  { path: '/auth', element: <AuthLayout />, children: [] },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [{ path: 'login', element: <Login /> }],
+  },
   {
     /* path: '/',
     element: <MainLayout />,
