9887ce50bde5b064b07a6a7e9709d53ff187242b
claramarsango <claraxmarsango@gmail.com>
2023-03-16 18:43:44 +0100

    change type of registerUser parameter

--- src/features/users/auth/auth-api.ts
+++ src/features/users/auth/auth-api.ts
@@ -1,6 +1,23 @@
-import { UserCredentials } from '../../../models/user-model';
+import {
+  UserLoginCredentials,
+  UserRegisterCredentials,
+} from '../../../models/user-model';
 
-export const logInUser = async (userToLog: UserCredentials) => {
+export const registerUser = async (userToRegister: UserRegisterCredentials) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}auth/register`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userToRegister),
+    },
+  );
+  return response;
+};
+
+export const logInUser = async (userToLog: UserLoginCredentials) => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}auth/login`, {
     method: 'POST',
     headers: {
