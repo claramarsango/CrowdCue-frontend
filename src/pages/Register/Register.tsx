d99dd2c7de8db7faf80efdbd071256fc60f869b0
claramarsango <claraxmarsango@gmail.com>
2023-03-16 19:18:43 +0100

    import register form component in register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,11 @@
+import RegisterForm from '../../features/users/auth/components/register-form/RegisterForm';
 import { PageTitle } from '../../shared/styles/PageTitleStyled';
 
 const Register = () => {
   return (
     <>
       <PageTitle data-testid="heading">REGISTER</PageTitle>
+      <RegisterForm />
     </>
   );
 };
