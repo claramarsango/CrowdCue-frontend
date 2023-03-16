6834f10dd35ea83f6675418f155c863dcbcad520
claramarsango <claraxmarsango@gmail.com>
2023-03-16 20:16:42 +0100

    add register user function and extra reducers to auth slice

--- src/features/users/auth/auth-slice.ts
+++ src/features/users/auth/auth-slice.ts
@@ -1,19 +1,50 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { AuthResponse, UserCredentials } from '../../../models/user-model';
+import {
+  AuthResponse,
+  UserLoginCredentials,
+  UserRegisterCredentials,
+} from '../../../models/user-model';
 import { RootState } from '../../../app/store';
-import { logInUser } from './auth-api';
+import { logInUser, registerUser } from './auth-api';
+
+export type AuthStatus = 'idle' | 'success' | 'error';
 
 export interface AuthFormState {
-  authState: 'idle' | 'success' | 'error';
+  registerState: AuthStatus;
+  loginState: AuthStatus;
   status: 'idle' | 'loading' | 'failed';
-  authMsg: string;
+  registerMsg: string;
+  loginMsg: string;
 }
 
 const initialState: AuthFormState = {
-  authState: 'idle',
+  registerState: 'idle',
+  loginState: 'idle',
   status: 'idle',
-  authMsg: '',
+  registerMsg: '',
+  loginMsg: '',
 };
+
+export const checkForRegisteredUser = createAsyncThunk(
+  'registerForm/fetch',
+
+  async (registerForm: HTMLFormElement) => {
+    const formData = new FormData(registerForm);
+
+    const userToRegister = Object.fromEntries(formData.entries());
+
+    const response = await registerUser(
+      userToRegister as UserRegisterCredentials,
+    );
+    const apiRes: AuthResponse = await response.json();
+
+    if (!response.ok) {
+      throw new Error(apiRes.msg);
+    }
+
+    return apiRes;
+  },
+);
 
 export const checkForExistingUser = createAsyncThunk(
   'loginForm/fetchToken',
@@ -22,7 +53,7 @@ export const checkForExistingUser = createAsyncThunk(
 
     const userToLog = Object.fromEntries(formData.entries());
 
-    const response = await logInUser(userToLog as UserCredentials);
+    const response = await logInUser(userToLog as UserLoginCredentials);
     const apiResUserData: AuthResponse = await response.json();
 
     if (!response.ok) {
@@ -40,22 +71,36 @@ export const authFormSlice = createSlice({
 
   extraReducers: builder => {
     builder
+      .addCase(checkForRegisteredUser.pending, state => {
+        state.status = 'loading';
+        state.loginState = 'idle';
+      })
+      .addCase(checkForRegisteredUser.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.registerState = 'success';
+      })
+      .addCase(checkForRegisteredUser.rejected, (state, action: any) => {
+        state.status = 'failed';
+        state.registerState = 'error';
+        state.registerMsg = action.error.message;
+      })
+
       .addCase(checkForExistingUser.pending, state => {
         state.status = 'loading';
-        state.authState = 'idle';
+        state.loginState = 'idle';
       })
       .addCase(
         checkForExistingUser.fulfilled,
         (state, action: PayloadAction<AuthResponse>) => {
           state.status = 'idle';
-          state.authState = 'success';
+          state.loginState = 'success';
           sessionStorage.setItem('Bearer', action.payload.accessToken);
         },
       )
       .addCase(checkForExistingUser.rejected, (state, action: any) => {
         state.status = 'failed';
-        state.authState = 'error';
-        state.authMsg = action.error.message;
+        state.loginState = 'error';
+        state.loginMsg = action.error.message;
       });
   },
 });
