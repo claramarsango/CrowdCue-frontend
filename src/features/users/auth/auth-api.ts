80d8b51077a9de4791ccc360265b4c649a353d29
claramarsango <claraxmarsango@gmail.com>
2023-03-16 16:42:01 +0100

    use environment variable in fetch and handlers

--- src/features/users/auth/auth-api.ts
+++ src/features/users/auth/auth-api.ts
@@ -1,17 +1,12 @@
 import { UserCredentials } from '../../../models/user-model';
 
-const logInUser = async (userToLog: UserCredentials) => {
-  const response = await fetch(
-    'https://clara-marsango-final-project-back-202301.onrender.com/auth/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userToLog),
+export const logInUser = async (userToLog: UserCredentials) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify(userToLog),
+  });
   return response;
 };
-
-export default logInUser;
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,15 +10,12 @@ export const handlers = [
 ];
 
 export const errorHandlers = [
-  rest.post(
-    'https://clara-marsango-final-project-back-202301.onrender.com/auth/login',
-    (_req, res, ctx) => {
-      return res.once(
-        ctx.status(404),
-        ctx.json({
-          msg: 'There is no registered user with this email and password',
-        }),
-      );
-    },
-  ),
+  rest.post(`${process.env.REACT_APP_API_URL}auth/login`, (_req, res, ctx) => {
+    return res.once(
+      ctx.status(404),
+      ctx.json({
+        msg: 'There is no registered user with this email and password',
+      }),
+    );
+  }),
 ];
