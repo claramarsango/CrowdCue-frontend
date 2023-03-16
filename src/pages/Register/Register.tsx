22a1c3ffc24a6c3f2b71d16bbb99c95690080fcd
claramarsango <claraxmarsango@gmail.com>
2023-03-16 16:10:12 +0100

    create register page with header and test

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from '../../shared/styles/PageTitleStyled';
+
+const Register = () => {
+  return (
+    <>
+      <PageTitle data-testid="heading">REGISTER</PageTitle>
+    </>
+  );
+};
+
+export default Register;
