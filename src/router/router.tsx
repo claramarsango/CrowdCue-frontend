6320115df200dc473daf188f2ab069b5a1504183
claramarsango <claraxmarsango@gmail.com>
2023-03-16 16:14:01 +0100

    add register page to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AuthLayout from '../layout/AuthLayout/AuthLayout';
 import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: 'login', element: <Login /> }],
+    children: [
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
+    ],
   },
 ]);
