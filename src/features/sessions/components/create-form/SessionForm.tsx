a0a10af77d86cad1ddb8bac473d0bdbb562c6552
claramarsango <claraxmarsango@gmail.com>
2023-03-20 18:02:37 +0100

    create session form component and test

---
+++ src/features/sessions/components/create-form/SessionForm.tsx
@@ -0,0 +1,65 @@
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import { createSessionAsync, selectSubmitState } from '../../sessions-slice';
+import { CreateSessionForm } from './session-form-styled';
+
+const SessionForm = () => {
+  const submitState = useAppSelector(selectSubmitState);
+  const { createSessionState, createSessionMsg } = submitState;
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      <CreateSessionForm
+        onSubmit={event => {
+          event.preventDefault();
+          dispatch(createSessionAsync(event.currentTarget));
+        }}
+      >
+        <div className="form__input-fields">
+          <div className="input-fields__image">
+            <div className="image__input-container">
+              <input
+                className="image__input"
+                type="file"
+                id="session-cover"
+                name="session-cover"
+                accept="image/png, image/jpeg"
+                data-testid="img-input"
+              />
+            </div>
+            <label htmlFor="session-cover" className="image__label">
+              upload an image
+            </label>
+          </div>
+
+          <div className="input-fields__title">
+            <label htmlFor="title" className="title__label">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="title__input"
+              required
+            />
+            <p
+              className={`form__error ${
+                createSessionState === 'error' ? '' : 'form__error--hidden'
+              }`}
+              role="paragraph"
+            >
+              {createSessionMsg}
+            </p>
+          </div>
+        </div>
+
+        <button className="form__submit-button" type="submit">
+          Start session
+        </button>
+      </CreateSessionForm>
+    </>
+  );
+};
+
+export default SessionForm;
