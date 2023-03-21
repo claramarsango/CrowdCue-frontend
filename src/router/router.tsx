c5d37fc8783936f9ceab17bc281db47a2468002e
claramarsango <claraxmarsango@gmail.com>
2023-03-21 12:44:08 +0100

    create explore page with test and add to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import AuthLayout from '../layout/AuthLayout/AuthLayout';
 import MainLayout from '../layout/MainLayout/MainLayout';
 import CreateSession from '../pages/CreateSession/CreateSession';
+import ExploreSessions from '../pages/ExploreSessions/ExploreSessions';
 import Login from '../pages/Login/Login';
 import Register from '../pages/Register/Register';
 
@@ -9,7 +10,10 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    children: [{ path: 'sessions', element: <CreateSession /> }],
+    children: [
+      { path: '', element: <ExploreSessions /> },
+      { path: 'sessions', element: <CreateSession /> },
+    ],
   },
   {
     path: 'auth/',
