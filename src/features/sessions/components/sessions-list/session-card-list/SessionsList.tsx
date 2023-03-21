afcd9013312392e6e7bd09eebf00ab0787e06dc4
claramarsango <claraxmarsango@gmail.com>
2023-03-21 18:37:28 +0100

    create variations of session list renderisation according to status

--- src/features/sessions/components/sessions-list/session-card-list/SessionsList.tsx
+++ src/features/sessions/components/sessions-list/session-card-list/SessionsList.tsx
@@ -3,12 +3,15 @@ import { useAppDispatch, useAppSelector } from '../../../../../app/hooks';
 import { sessionResponse } from '../../../../../models/session-model';
 import Loading from '../../../../../shared/components/Loading';
 import { getSessionsAsync, selectSessionState } from '../../../sessions-slice';
+import { SessionCardContainer } from '../../session-card/session-card-styled';
 import SessionCard from '../../session-card/SessionCard';
-import { SessionsListContainer } from './session-list-styled';
+import { ListFeedback, SessionsListContainer } from './session-list-styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const SessionsList = () => {
   const sessionListState = useAppSelector(selectSessionState);
-  const { status, previewSessions } = sessionListState;
+  const { status, previewSessions, sessionMsg } = sessionListState;
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -18,17 +21,29 @@ const SessionsList = () => {
   const generateSessionsList = () => {
     switch (status) {
       case 'loading':
-        return <Loading />;
+        return (
+          <>
+            <Loading container="page" />
+            <h3>Loading sessions</h3>
+          </>
+        );
       case 'failed':
         return (
-          <h2>Error</h2>
-          //redirect to 500 page
+          <ListFeedback>
+            <FontAwesomeIcon
+              icon={solid('circle-exclamation')}
+              className="error-icon"
+            />
+            <h3 className="error-message">{sessionMsg}</h3>
+          </ListFeedback>
         );
       default:
         return (
           <SessionsListContainer>
             {previewSessions.map((session: sessionResponse) => (
-              <SessionCard session={session} />
+              <SessionCardContainer key={`session-id-${session._id}`}>
+                <SessionCard session={session} />
+              </SessionCardContainer>
             ))}
           </SessionsListContainer>
         );
--- src/features/sessions/components/sessions-list/session-card-list/session-list-styled.ts
+++ src/features/sessions/components/sessions-list/session-card-list/session-list-styled.ts
@@ -7,4 +7,28 @@ export const SessionsListContainer = styled.ul`
   align-content: space-between;
   row-gap: 2rem;
   padding: 1.75rem;
+
+  @media (min-width: 1440px) {
+    padding: 3rem 8rem;
+    row-gap: 4rem;
+    column-gap: 5rem;
+  }
+`;
+
+export const ListFeedback = styled.div`
+  padding: 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  .error-icon {
+    width: 10rem;
+    height: 10rem;
+    margin: auto;
+  }
+
+  .error-message {
+    font-size: var(--mobile-text-size-l);
+    font-weight: var(--text-regular);
+  }
 `;
