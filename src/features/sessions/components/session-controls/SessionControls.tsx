7157f6f1183bdd845084360dd402502df9f9dee9
claramarsango <claraxmarsango@gmail.com>
2023-03-22 21:00:07 +0100

    add loading and failed cases to session controls component

--- src/features/sessions/components/session-controls/SessionControls.tsx
+++ src/features/sessions/components/session-controls/SessionControls.tsx
@@ -1,5 +1,7 @@
 import { FC, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+import ErrorFeedback from '../../../../shared/components/error-feedback/ErrorFeedback';
+import Loading from '../../../../shared/components/loading/Loading';
 import {
   getSessionDetailAsync,
   selectSessionState,
@@ -13,7 +15,7 @@ interface SessionControlsProps {
 
 const SessionControls: FC<SessionControlsProps> = ({ sessionId }) => {
   const sessionDetailState = useAppSelector(selectSessionState);
-  const { session } = sessionDetailState;
+  const { session, status } = sessionDetailState;
   const { title, participants } = session;
   const dispatch = useAppDispatch();
 
@@ -21,30 +23,50 @@ const SessionControls: FC<SessionControlsProps> = ({ sessionId }) => {
     dispatch(getSessionDetailAsync(sessionId));
   }, [dispatch, sessionId]);
 
-  return (
-    <>
-      <SectionTitle>{title}</SectionTitle>
-      <SessionControlsStyled>
-        <p className="participants">
-          There are {participants.length} people in this session
-        </p>
-        <div className="player">
-          <p className="player__message">
-            There are no songs playing currently
-          </p>
-        </div>
-        <p className="session-url">
-          <span className="url__title">URL</span>:
-          {`  ${process.env.REACT_APP_API_URL}api/v1/sessions/${sessionId}`}
-        </p>
-        <div className="control-buttons">
-          <button className="buttons__search">Search</button>
-          <button className="buttons__queue">Queue</button>
-          <button className="buttons__quit">Leave session</button>
-        </div>
-      </SessionControlsStyled>
-    </>
-  );
+  const generateSessionDetail = () => {
+    switch (status) {
+      case 'loading':
+        return (
+          <>
+            <Loading container="page" />
+            <h3>Loading session</h3>
+          </>
+        );
+      case 'failed':
+        return (
+          <>
+            <ErrorFeedback />
+          </>
+        );
+      default:
+        return (
+          <>
+            <SectionTitle>{title}</SectionTitle>
+            <SessionControlsStyled>
+              <p className="participants">
+                There are {participants.length} people in this session
+              </p>
+              <div className="player">
+                <p className="player__message">
+                  There are no songs playing currently
+                </p>
+              </div>
+              <p className="session-url">
+                <span className="url__title">URL</span>:
+                {`  ${process.env.REACT_APP_API_URL}api/v1/sessions/${sessionId}`}
+              </p>
+              <div className="control-buttons">
+                <button className="buttons__search">Search</button>
+                <button className="buttons__queue">Queue</button>
+                <button className="buttons__quit">Leave session</button>
+              </div>
+            </SessionControlsStyled>
+          </>
+        );
+    }
+  };
+
+  return <>{generateSessionDetail()}</>;
 };
 
 export default SessionControls;
