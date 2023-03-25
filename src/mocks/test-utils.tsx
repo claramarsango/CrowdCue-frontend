07915c9dd88ccf25b2b6aab162930ea01309baf9
claramarsango <claraxmarsango@gmail.com>
2023-03-25 15:59:33 +0100

    set up reusable test render function

---
+++ src/mocks/test-utils.tsx
@@ -0,0 +1,50 @@
+import { PreloadedState } from '@reduxjs/toolkit';
+import { render, RenderOptions } from '@testing-library/react';
+import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+import { AppStore, RootState, setupStore } from '../app/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState = {
+      authForm: {
+        registerState: 'idle',
+        loginState: 'idle',
+        status: 'idle',
+        registerMsg: '',
+        loginMsg: '',
+      },
+      sessionComponent: {
+        status: 'idle',
+        createSessionState: 'idle',
+        sessionMsg: '',
+        previewSessions: [],
+        session: {
+          title: '',
+          coverImageURL: '',
+          url: '',
+          currentSong: '',
+          queuedSongs: [],
+          admin: '',
+          participants: [],
+          _id: 0,
+        },
+        deleteStatus: 'idle',
+      },
+    },
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {},
+) => {
+  const Wrapper = ({ children }: PropsWithChildren<{}>): JSX.Element => {
+    return <Provider store={store}>{children}</Provider>;
+  };
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
