d0bf543e395e32509344ff41c64032ca43aac917
claramarsango <claraxmarsango@gmail.com>
2023-03-29 18:30:00 +0200

    add redirect button to error feedback and styles

--- src/shared/components/error-feedback/ErrorFeedback.tsx
+++ src/shared/components/error-feedback/ErrorFeedback.tsx
@@ -1,23 +1,36 @@
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useAppSelector } from '../../../app/hooks';
-import { selectSessionState } from '../../../features/sessions/sessions-slice';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import {
+  restoreAllStatus,
+  selectSessionState,
+} from '../../../features/sessions/sessions-slice';
 import { ErrorFeedbackStyled } from './error-feedback-styled';
 
 const ErrorFeedback = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const sessionListState = useAppSelector(selectSessionState);
   const { sessionMsg } = sessionListState;
 
   return (
-    <>
-      <ErrorFeedbackStyled>
-        <FontAwesomeIcon
-          icon={solid('circle-exclamation')}
-          className="error-icon"
-        />
-        <h3 className="error-message">{sessionMsg}</h3>
-      </ErrorFeedbackStyled>
-    </>
+    <ErrorFeedbackStyled>
+      <FontAwesomeIcon
+        icon={solid('circle-exclamation')}
+        className="error-icon"
+      />
+      <h3 className="error-message">{sessionMsg}</h3>
+      <button
+        className="error-redirect-button"
+        onClick={() => {
+          dispatch(restoreAllStatus());
+          navigate('/');
+        }}
+      >
+        Go back home
+      </button>
+    </ErrorFeedbackStyled>
   );
 };
 
